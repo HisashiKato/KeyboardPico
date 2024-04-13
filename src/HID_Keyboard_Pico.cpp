f07ed4137f3c01522280917cd5b236f6837549ce
HisashiKato <68432957+HisashiKato@users.noreply.github.com>
2024-04-13 18:02:54 +0900

    Tab to Space

--- src/HID_Keyboard_Pico.cpp
+++ src/HID_Keyboard_Pico.cpp
@@ -51,46 +51,46 @@ void HID_Keyboard_Pico::end(void) {
 // call release(), releaseAll(), or otherwise clear the report and resend.
 size_t HID_Keyboard_Pico::press(uint8_t k)
 {
-	uint8_t i;
-	uint16_t key;
+    uint8_t i;
+    uint16_t key;
 
     if (k > 0xE7) {
-		setWriteError();
-		return 0;
-	} else if (k >= 0xE0) {	// it's a modifier key
-		_keyReport.modifiers |= (1<<(k-0xE0));
-		key = 0;
-	} else {				// it's a printing key
-		key = pgm_read_word(_asciimap + k);
-		if (!key) {
-			setWriteError();
-			return 0;
-		}
+        setWriteError();
+        return 0;
+    } else if (k >= 0xE0) { // it's a modifier key
+        _keyReport.modifiers |= (1<<(k-0xE0));
+        key = 0;
+    } else {                // it's a printing key
+        key = pgm_read_word(_asciimap + k);
+        if (!key) {
+            setWriteError();
+            return 0;
+        }
         if ((key & SHIFT) == SHIFT) {
-			_keyReport.modifiers |= 0x02;	// the left shift modifier
-			key &= 0x00FF;
-		}
-	}
-	k = (uint8_t)key;
-	// Add k to the key report only if it's not already present
-	// and if there is an empty slot.
-	if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
-		_keyReport.keys[2] != k && _keyReport.keys[3] != k &&
-		_keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+            _keyReport.modifiers |= 0x02;   // the left shift modifier
+            key &= 0x00FF;
+        }
+    }
+    k = (uint8_t)key;
+    // Add k to the key report only if it's not already present
+    // and if there is an empty slot.
+    if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
+        _keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+        _keyReport.keys[4] != k && _keyReport.keys[5] != k) {
 
-		for (i = 0; i < 6; i++) {
-			if (_keyReport.keys[i] == 0x00) {
-				_keyReport.keys[i] = k;
-				break;
-			}
-		}
-		if (i == 6) {
-			setWriteError();
-			return 0;
-		}
-	}
-	sendReport(&_keyReport);
-	return 1;
+        for (i = 0; i < 6; i++) {
+            if (_keyReport.keys[i] == 0x00) {
+                _keyReport.keys[i] = k;
+                break;
+            }
+        }
+        if (i == 6) {
+            setWriteError();
+            return 0;
+        }
+    }
+    sendReport(&_keyReport);
+    return 1;
 }
 
 // release() takes the specified key out of the persistent key report and
@@ -98,67 +98,67 @@ size_t HID_Keyboard_Pico::press(uint8_t k)
 // it shouldn't be repeated any more.
 size_t HID_Keyboard_Pico::release(uint8_t k)
 {
-	uint8_t i;
-	uint16_t key;
+    uint8_t i;
+    uint16_t key;
 
     if (k > 0xE7) {
-		setWriteError();
-		return 0;
-	} else if (k >= 0xE0) {	// it's a modifier key
-		_keyReport.modifiers &= ~(1<<(k-0xE0));
-		key = 0;
-	} else {				// it's a printing key
-		key = pgm_read_word(_asciimap + k);
-		if (!key) {
-			return 0;
-		}
-		if ((key & SHIFT) == SHIFT) {
-			_keyReport.modifiers &= ~(0x02);	// the left shift modifier
-			key &= 0x00FF;
-		}
-	}
-	k = (uint8_t)key;
-	// Test the key report to see if k is present.  Clear it if it exists.
-	// Check all positions in case the key is present more than once (which it shouldn't be)
-	for (i = 0; i < 6; i++) {
-		if (0 != k && _keyReport.keys[i] == k) {
-			_keyReport.keys[i] = 0x00;
-		}
-	}
+        setWriteError();
+        return 0;
+    } else if (k >= 0xE0) { // it's a modifier key
+        _keyReport.modifiers &= ~(1<<(k-0xE0));
+        key = 0;
+    } else {                // it's a printing key
+        key = pgm_read_word(_asciimap + k);
+        if (!key) {
+            return 0;
+        }
+        if ((key & SHIFT) == SHIFT) {
+            _keyReport.modifiers &= ~(0x02);    // the left shift modifier
+            key &= 0x00FF;
+        }
+    }
+    k = (uint8_t)key;
+    // Test the key report to see if k is present.  Clear it if it exists.
+    // Check all positions in case the key is present more than once (which it shouldn't be)
+    for (i = 0; i < 6; i++) {
+        if (0 != k && _keyReport.keys[i] == k) {
+            _keyReport.keys[i] = 0x00;
+        }
+    }
 
-	sendReport(&_keyReport);
-	return 1;
+    sendReport(&_keyReport);
+    return 1;
 }
 size_t HID_Keyboard_Pico::pressID(uint8_t k)
 {
-	uint8_t i;
+    uint8_t i;
 
     if (k > 0xE7) {
-		setWriteError();
-		return 0;
-	} else if (k >= 0xE0) {	// it's a modifier key
-		_keyReport.modifiers |= (1<<(k-0xE0));
-		k = 0;
-	}
-	// Add k to the key report only if it's not already present
-	// and if there is an empty slot.
-	if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
-		_keyReport.keys[2] != k && _keyReport.keys[3] != k &&
-		_keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+        setWriteError();
+        return 0;
+    } else if (k >= 0xE0) { // it's a modifier key
+        _keyReport.modifiers |= (1<<(k-0xE0));
+        k = 0;
+    }
+    // Add k to the key report only if it's not already present
+    // and if there is an empty slot.
+    if (_keyReport.keys[0] != k && _keyReport.keys[1] != k &&
+        _keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+        _keyReport.keys[4] != k && _keyReport.keys[5] != k) {
 
-		for (i = 0; i < 6; i++) {
-			if (_keyReport.keys[i] == 0x00) {
-				_keyReport.keys[i] = k;
-				break;
-			}
-		}
-		if (i == 6) {
-			setWriteError();
-			return 0;
-		}
-	}
-	sendReport(&_keyReport);
-	return 1;
+        for (i = 0; i < 6; i++) {
+            if (_keyReport.keys[i] == 0x00) {
+                _keyReport.keys[i] = k;
+                break;
+            }
+        }
+        if (i == 6) {
+            setWriteError();
+            return 0;
+        }
+    }
+    sendReport(&_keyReport);
+    return 1;
 }
 
 // release() takes the specified key out of the persistent key report and
@@ -166,25 +166,25 @@ size_t HID_Keyboard_Pico::pressID(uint8_t k)
 // it shouldn't be repeated any more.
 size_t HID_Keyboard_Pico::releaseID(uint8_t k)
 {
-	uint8_t i;
+    uint8_t i;
 
     if (k > 0xE7) {
-		setWriteError();
-		return 0;
-	} else if (k >= 0xE0) {	// it's a modifier key
-		_keyReport.modifiers &= ~(1<<(k-0xE0));
-		k = 0;
-	}
-	// Test the key report to see if k is present.  Clear it if it exists.
-	// Check all positions in case the key is present more than once (which it shouldn't be)
-	for (i = 0; i < 6; i++) {
-		if (0 != k && _keyReport.keys[i] == k) {
-			_keyReport.keys[i] = 0x00;
-		}
-	}
+        setWriteError();
+        return 0;
+    } else if (k >= 0xE0) { // it's a modifier key
+        _keyReport.modifiers &= ~(1<<(k-0xE0));
+        k = 0;
+    }
+    // Test the key report to see if k is present.  Clear it if it exists.
+    // Check all positions in case the key is present more than once (which it shouldn't be)
+    for (i = 0; i < 6; i++) {
+        if (0 != k && _keyReport.keys[i] == k) {
+            _keyReport.keys[i] = 0x00;
+        }
+    }
 
-	sendReport(&_keyReport);
-	return 1;
+    sendReport(&_keyReport);
+    return 1;
 }
 
 size_t HID_Keyboard_Pico::consumerPress(uint16_t k)
@@ -201,47 +201,47 @@ size_t HID_Keyboard_Pico::consumerRelease()
 
 void HID_Keyboard_Pico::releaseAll(void)
 {
-	_keyReport.keys[0] = 0;
-	_keyReport.keys[1] = 0;
-	_keyReport.keys[2] = 0;
-	_keyReport.keys[3] = 0;
-	_keyReport.keys[4] = 0;
-	_keyReport.keys[5] = 0;
-	_keyReport.modifiers = 0;
-	sendReport(&_keyReport);
+    _keyReport.keys[0] = 0;
+    _keyReport.keys[1] = 0;
+    _keyReport.keys[2] = 0;
+    _keyReport.keys[3] = 0;
+    _keyReport.keys[4] = 0;
+    _keyReport.keys[5] = 0;
+    _keyReport.modifiers = 0;
+    sendReport(&_keyReport);
 }
 
 size_t HID_Keyboard_Pico::write(uint8_t c)
 {
-	uint8_t p = press(c);  // Keydown
-	delay(10);
-	release(c);            // Keyup
-	delay(10);
-	return p;              // just return the result of press() since release() almost always returns 1
+    uint8_t p = press(c);  // Keydown
+    delay(10);
+    release(c);            // Keyup
+    delay(10);
+    return p;              // just return the result of press() since release() almost always returns 1
 }
 
 size_t HID_Keyboard_Pico::write(const uint8_t *buffer, size_t size) {
-	size_t n = 0;
-	while (size--) {
-		if (*buffer != '\r') {
-			if (write(*buffer)) {
-				n++;
-			} else {
-				break;
-			}
-		}
-		buffer++;
-	}
-	return n;
+    size_t n = 0;
+    while (size--) {
+        if (*buffer != '\r') {
+            if (write(*buffer)) {
+                n++;
+            } else {
+                break;
+            }
+        }
+        buffer++;
+    }
+    return n;
 }
 
 size_t HID_Keyboard_Pico::writeID(uint8_t k)
 {
-	uint8_t p = pressID(k);  // Keydown
-	delay(10);
-	releaseID(k);            // Keyup
-	delay(10);
-	return p;              // just return the result of press() since release() almost always returns 1
+    uint8_t p = pressID(k);  // Keydown
+    delay(10);
+    releaseID(k);            // Keyup
+    delay(10);
+    return p;              // just return the result of press() since release() almost always returns 1
 }
 
 void HID_Keyboard_Pico::onLED(LedCallbackFcn fcn, void *cbData) {
--- src/HID_Keyboard_Pico.h
+++ src/HID_Keyboard_Pico.h
@@ -28,38 +28,36 @@
 #include "KeyboardLayout.h"
 
 // Low level key report: up to 6 keys and shift, ctrl etc at once
-typedef struct
-{
-  uint8_t modifiers;
-  uint8_t reserved;
-  uint8_t keys[6];
+typedef struct {
+    uint8_t modifiers;
+    uint8_t reserved;
+    uint8_t keys[6];
 } KeyReport;
 
-class HID_Keyboard_Pico : public Print
-{
+class HID_Keyboard_Pico : public Print {
 protected:
-  KeyReport _keyReport;
-  const uint16_t *_asciimap;
-  virtual void sendReport(KeyReport* keys) = 0;
-  virtual void sendConsumerReport(uint16_t key) = 0;
+    KeyReport _keyReport;
+    const uint16_t *_asciimap;
+    virtual void sendReport(KeyReport* keys) = 0;
+    virtual void sendConsumerReport(uint16_t key) = 0;
 
 public:
-  HID_Keyboard_Pico(void);
-  void begin(const uint16_t *layout = KeyboardLayout_en_US);
-  void end(void);
-  size_t write(uint8_t c);
-  size_t write(const uint8_t *buffer, size_t size);
-  size_t press(uint8_t k);
-  size_t release(uint8_t k);
-  size_t writeID(uint8_t k);
-  size_t pressID(uint8_t k);
-  size_t releaseID(uint8_t k);
-  void releaseAll(void);
-  size_t consumerPress(uint16_t k);
-  size_t consumerRelease();
+    HID_Keyboard_Pico(void);
+    void begin(const uint16_t *layout = KeyboardLayout_en_US);
+    void end(void);
+    size_t write(uint8_t c);
+    size_t write(const uint8_t *buffer, size_t size);
+    size_t press(uint8_t k);
+    size_t release(uint8_t k);
+    size_t writeID(uint8_t k);
+    size_t pressID(uint8_t k);
+    size_t releaseID(uint8_t k);
+    void releaseAll(void);
+    size_t consumerPress(uint16_t k);
+    size_t consumerRelease();
 
-  typedef void(*LedCallbackFcn)(bool numlock, bool capslock, bool scrolllock, bool compose, bool kana, void *cbData);
-  void onLED(LedCallbackFcn fcn, void *cbData = nullptr);
-  LedCallbackFcn _ledCB;
-  void * _ledCBdata;
+    typedef void(*LedCallbackFcn)(bool numlock, bool capslock, bool scrolllock, bool compose, bool kana, void *cbData);
+    void onLED(LedCallbackFcn fcn, void *cbData = nullptr);
+    LedCallbackFcn _ledCB;
+    void * _ledCBdata;
 };
